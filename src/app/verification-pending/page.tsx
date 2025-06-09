4127d6f893db58c2202775d62dbb82801e1b72fc
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-06-09 11:25:17 +0530

    No verify

--- src/app/verification-pending/page.tsx
+++ src/app/verification-pending/page.tsx
@@ -6,12 +6,12 @@ import AuthContext from "@/context/AuthContext";
 import OtpVerification from "@/components/Login/otp-verification";
 
 export default function Verification() {
-  const { user } = useContext(AuthContext);
-  const router = useRouter();
+  // const { user } = useContext(AuthContext);
+  // const router = useRouter();
 
-  if (!user) {
-    router.push("/auth");
-  }
+  // if (!user) {
+  //   router.push("/auth");
+  // }
   return (
     <>
       <div className="flex h-screen relative w-full m-0 p-0 justify-center items-center flex-col overflow-hidden">

5ed08c3ba458021d3218f28f97b2e3d0e81c3b4e
Jacob-alwin <alwinjacobjoy@gmail.com>
2025-07-02 09:34:01 +0530

    Log

--- src/app/dashboard/logistics/page.tsx
+++ src/app/dashboard/logistics/page.tsx
@@ -13,7 +13,7 @@ export default function CIFPage() {
     originLat: "",
     originLon: "",
     originPort: "",
-    destinationLQon: "",
+    destinationLon: "",
     destinationLat: "",
     destinationPort: "",
     destPort: "",
